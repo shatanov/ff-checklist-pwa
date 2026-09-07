43320d60e828d1734d1bb355f8036b5588d9ef69
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2026-09-07 17:00:52 +0300

    Restore PWA cache version 9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='ff-checklist-v10';
+var CACHE='ff-checklist-v9';
 var APP=['./','./index.html','./data.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(APP);}));self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}));self.clients.claim();});
